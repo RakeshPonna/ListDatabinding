9303c6b20f5319b774d821d4fadc3873e996da16
RakeshPonna <Rakesh@2791>
2019-11-29 13:40:36 +0530

    List databinding with recyclerview

---
+++ app/src/main/java/com/rakesh/listdatabinding/EmployeeDataService.java
@@ -0,0 +1,9 @@
+package com.rakesh.listdatabinding;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EmployeeDataService {
+  @GET("users/?per_page=12&amp;page=1")
+  Call<EmployeeDBResponse> getEmployees();
+}
